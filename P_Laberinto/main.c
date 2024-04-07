7d9576de6990d2cc4d149775a5e4f48bf4cdfd76
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2024-04-06 23:37:01 -0600

    Version 1.0

--- P_Laberinto/main.c
+++ P_Laberinto/main.c
@@ -1,215 +1,149 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
-#include <pthread.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-#define MAX_ROWS 100
-#define MAX_COLS 100
+// Definimos tamaño de la matriz
+#define ROWS 8
+#define COLS 8
 
-//PROTOTIPOS
-void *traverseMatrix(void *arg);
+int leer_matrix_file(const char* filename, char matrix[ROWS][COLS]);
+
+// Nombre del archivo
+const char* filename = "/home/stef/Desktop/Git/SO_S1_2024/laberinto.txt";
+
+// Definición del laberinto
+char laberinto[ROWS][COLS];
+
+// Matriz para registrar celdas visitadas por cada hilo
+int visitado[ROWS][COLS] = {0};
+
+// Mutex para sincronización
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // Función para leer datos desde un archivo y cargarlos en una matriz
-int readMatrixFromFile(const char* filename, char matrix[MAX_ROWS][MAX_COLS], int *rows, int *cols) {
+int leer_matrix_file(const char* filename, char matrix[ROWS][COLS]) {
+    int rows = 0, cols = 0; // Inicializar las variables para el número de filas y columnas
+    int MAX_ROWS = 100,MAX_COLS = 100;
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error al abrir el archivo.\n");
         return -1;
     }
 
-    *rows = 0;
-    *cols = 0;
-
-    // Leer el archivo línea por línea
     char line[MAX_COLS];
-    while (fgets(line, sizeof(line), file) && *rows < MAX_ROWS) {
-        // Recorrer cada caracter de la línea
+    while (fgets(line, sizeof(line), file) && rows < MAX_ROWS) {
         int col = 0;
         for (int i = 0; line[i] != '\0' && col < MAX_COLS; i++) {
-            // Si el caracter es un asterisco o un espacio, se guarda en la matriz
-            if (line[i] == '*' || line[i] == ' ' || line[i] == '/' || line[i] == 'x') {
-                matrix[*rows][col] = line[i];
+            // Del archivo se leen solo los siguientes caracteres, esto permite tener las ',' como separadores
+            if (line[i] == '*' || line[i] == ' ' || line[i] == '/' || line[i] == 'x'){
+                matrix[rows][col] = line[i]; // Almacenamos datos en la matriz
                 col++;
             }
         }
-        // Actualizar el número de columnas si es necesario
-        if (col > *cols) {
-            *cols = col;
+        if (col > cols) {
+            cols = col;
         }
-        (*rows)++;
+        (rows)++;
     }
 
     fclose(file);
     return 0;
 }
 
-// Función para imprimir la matriz
-void printMatrix(char matrix[MAX_ROWS][MAX_COLS], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%c ", matrix[i][j]);
+// Estructura para pasar información a los hilos
+typedef struct {
+    int row;
+    int col;
+    int pasos;
+} ThreadArgs;
+
+// Función para mostrar el laberinto
+void print_laberinto() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%c ", laberinto[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-// Estructura para los argumentos del hilo
-typedef struct {
-    char matrix[MAX_ROWS][MAX_COLS];
-    int rows;
-    int cols;
-    int x ; // Posición inicial en x
-    int y ; // Posición inicial en y
-    int dx; // Dirección de movimiento en x (1 para la derecha, -1 para la izquierda)
-    int dy;
-    char simbolo; 
-} ThreadArgs;
-
-
-void createNewThread(ThreadArgs *args,char new_simbol) {
-    pthread_t thread_id;
-    //printf("Creating new thread to continue in direction (%d, %d)\n", args->dx, args->dy);
-    
-    // Copiar la matriz al argumento del hilo
-    ThreadArgs new_args = *args;
-    new_args.simbolo = new_simbol;
-    
-    pthread_create(&thread_id, NULL, traverseMatrix, (void *)&new_args);
-    pthread_join(thread_id, NULL);
+// Función para validar si una posición es válida
+int is_valid(int row, int col) {
+    return row >= 0 && row < ROWS && col >= 0 && col < COLS && laberinto[row][col] != '*' && !visitado[row][col];
 }
 
-// Función que el hilo ejecutará
-void *traverseMatrix(void *arg) {
-    ThreadArgs *args = (ThreadArgs *)arg;
+// Función para recorrido_threadr un hilo
+void recorrido_thread(ThreadArgs *args) {
+    int row = args->row;
+    int col = args->col;
+    int pasos = args->pasos;
 
-    //int x = 0; // Posición inicial en x
-    //int y = 0; // Posición inicial en y
-    //int dx = 1; // Dirección de movimiento en x (1 para la derecha, -1 para la izquierda)
-    //int dy = 0; // Dirección de movimiento en y (1 para abajo, -1 para arriba)
+    // Marcar la posición actual como visitada
+    visitado[row][col] = 1;
 
-    // Recorrer la matriz
-    while (1) {
-        // Poner 'x' en la casilla actual
-        args->matrix[args->y][args->x] = args->simbolo;
+    // Mostrar el laberinto
+    pthread_mutex_lock(&mutex);
+    system("clear");
+    printf("Cantidad de pasos necesarios: %d\n", pasos);
+    print_laberinto();
+    pthread_mutex_unlock(&mutex);
+    sleep(1);
 
-        // Comprobar si estamos fuera de los límites de la matriz
-        if (args->y == args->rows){
-            //tratar de ir hacia derecha
-            printf("llegue al final de abajo");
-            args->dx = 1;
-            args->dy = 0;
-            args->matrix[args->y][args->x] = args->simbolo;
-            args->x = args->x+1;
-            createNewThread(args,'O');
-
-            break;
-        }
-        if (args->x == args->cols) {
-            //tratar de ir hacie abajo
-            args->dx = 0;
-            args->dy = 1;
-            args->matrix[args->y][args->x] = args->simbolo;
-            args->x = args->y+1;
-            createNewThread(args,'C');
-            break;;
-        }
-        if(args->y-1 < 0){
-
-
-        }
-
-            
-        if (args->dx != 0) { // Si estábamos moviéndonos horizontalmente
-        // Comprobar si encontramos una pared ('*')
-            if (args->matrix[args->y][args->x+1] == '*') {
-                // Cambiar la dirección de movimiento
-                
-                    // Intentar moverse hacia abajo
-                    args->dx = 0;
-                    args->dy = 1;
-                    args->matrix[args->y][args->x] = args->simbolo;
-                    args->y = args->y +1;
-                    createNewThread(args,'C');
-                    return NULL;
-            
-                
-            }
-        }
-       
-        if (args->dy != 0) { // Si estábamos moviéndonos verticalmente
-            if (args->matrix[args->y+1][args->x] == '*') {
-                // Cambiar la dirección de movimiento
-                
-                // Intentar moverse hacia la derecha
-                args->dx = 1;
-                args->dy = 0;
-                args->matrix[args->y][args->x] = args->simbolo;
-                args->x = args->x+1;
-                createNewThread(args,'O');
-
-
-                return NULL;
-            } 
-        }
-        
-    
-     
-        // Imprimir la matriz modificada
-        system("clear"); // Limpiar la pantalla (compatible con sistemas Unix)
-        printf("Matriz modificada:\n");
-        printMatrix(args->matrix, args->rows, args->cols);
-        sleep(1); // Esperar un segundo antes de avanzar
-
-        // Moverse en la dirección actual
-        args->x += args->dx;
-        args->y += args->dy;
-
+    // Verificar la salida
+    if (laberinto[row][col] == '/') {
+        printf("Hilo exitoso! Pasos totales: %d\n", pasos);
+        pthread_exit(NULL);
     }
 
-    return NULL;
+    // Marcar la posición actual en el laberinto
+    laberinto[row][col] = 'x';
+
+    // Direcciones posibles
+    // d_row -1: izquierda
+    // d_row 1:  derecha
+    // d_col -1: arriba
+    // d_col 1:  abajo
+    int d_row[] = {-1, 1, 0, 0};
+    int d_col[] = {0, 0, -1, 1};
+
+    // Explorar las direcciones
+    for (int i = 0; i < 4; i++) {
+        int new_row = row + d_row[i];
+        int new_col = col + d_col[i];
+
+        if (is_valid(new_row, new_col)) {
+            // Crear un nuevo hilo
+            // Con sus respectivos cambios de direccion
+            ThreadArgs *new_args = (ThreadArgs *)malloc(sizeof(ThreadArgs));
+            new_args->row = new_row;
+            new_args->col = new_col;
+            new_args->pasos = pasos + 1;
+
+            pthread_t thread;
+            pthread_create(&thread, NULL, (void *(*)(void *))recorrido_thread, new_args);
+        }
+    }
+
+    // Finalizamos el hilo
+    pthread_exit(NULL);
 }
 
-
-
 int main() {
-    char matrix[MAX_ROWS][MAX_COLS];
-    int rows, cols;
-
-    // Nombre del archivo
-     const char* filename = "/Users/admin/Documents/1 Semestre 2024/SO/SO_S1_2024/laberinto.txt";
-
-    // Leer la matriz desde el archivo
-    if (readMatrixFromFile(filename, matrix, &rows, &cols) != 0) {
+    if (leer_matrix_file(filename,laberinto) != 0) {
         printf("Error al leer la matriz desde el archivo.\n");
         return 1;
     }
+    // Iniciar el movimiento desde la posición (0, 0)
+    ThreadArgs *args = (ThreadArgs *)malloc(sizeof(ThreadArgs));
+    args->row = 0;
+    args->col = 0;
+    args->pasos = 0;
 
-    // Imprimir la matriz
-    printf("Matriz leída desde el archivo:\n");
-    printMatrix(matrix, rows, cols);
+    recorrido_thread(args);
 
-    // Crear la estructura de argumentos para el hilo
-    ThreadArgs args = {
-        .rows = rows,
-        .cols = cols,
-        .x = 0, // Posición inicial en x
-        .y = 0, // Posición inicial en y
-        .dx = 1, // Dirección de movimiento en x (1 para la derecha, -1 para la izquierda)
-        .dy = 0,
-        .simbolo = 'X' 
-    };
-
-    // Copiar la matriz al argumento del hilo
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            args.matrix[i][j] = matrix[i][j];
-        }
-    }
-
-    pthread_t thread_id;
-    printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, traverseMatrix, (void *)&args);
-    pthread_join(thread_id, NULL);
-    printf("After Thread\n");
     return 0;
 }
+
+
